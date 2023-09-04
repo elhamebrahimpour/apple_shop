2b23405a7a762be3652d66f3e4c310577636218b
elham <ebrahimpourelham44@gmail.com>
2023-09-04 16:08:14 +0330

    rewrite splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,103 +6,154 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.blueColor,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 80, bottom: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return Container(
+      decoration: const BoxDecoration(
+          color: AppColors.blueColor,
+          image: DecorationImage(
+            repeat: ImageRepeat.repeat,
+            opacity: 0.6,
+            image: AssetImage('images/star_pattern.png'),
+          )),
+      child: const Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 32, right: 32, bottom: 32),
+            LogoWidget(),
+            SloganWidget(),
+            StartIcon(),
+            AppFooter(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppFooter extends StatelessWidget {
+  const AppFooter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 32),
+      width: double.infinity,
+      child: const Column(
+        children: [
+          Text(
+            'From',
+            style: TextStyle(
+              color: AppColors.whiteColor,
+              fontSize: 12,
+              fontFamily: 'sm',
+            ),
+          ),
+          Text(
+            'Shop',
+            style: TextStyle(
+              color: AppColors.whiteColor,
+              fontSize: 12,
+              fontFamily: 'sm',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StartIcon extends StatelessWidget {
+  const StartIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 22, bottom: 70),
+      height: 70,
+      width: 70,
+      decoration: const BoxDecoration(
+        color: AppColors.blue,
+        shape: BoxShape.circle,
+      ),
+      child: Image.asset('images/icon_right_arrow_cricle.png'),
+    );
+  }
+}
+
+class SloganWidget extends StatelessWidget {
+  const SloganWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            'اوج هیجان با خرید',
+            style: TextStyle(
+              color: AppColors.whiteColor,
+              fontSize: 30,
+              fontFamily: 'sb',
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            '!محصولات اپل',
+            style: TextStyle(
+              color: AppColors.whiteColor,
+              fontSize: 30,
+              fontFamily: 'sb',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset('images/splash_back.png'),
+            Positioned(
+              top: 170,
               child: SizedBox(
-                width: 340,
-                height: 340,
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Image.asset('images/splash_back.png'),
-                    Positioned(
-                      bottom: 148,
-                      child: SizedBox(
-                        height: 84,
-                        width: 100,
-                        child: Image.asset('images/icon_app.png'),
-                      ),
-                    ),
-                    const Positioned(
-                      bottom: 110,
-                      child: Text(
-                        'اپل شاپ',
-                        style: TextStyle(
-                          fontFamily: 'sb',
-                          fontSize: 20,
-                          color: AppColors.whiteColor,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+                height: 90,
+                width: 90,
+                child: Image.asset('images/icon_app.png'),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(right: 16, left: 210),
+            const Positioned(
+              bottom: 160,
               child: Text(
-                'اوج هیجان با',
+                'اپل شاپ',
                 style: TextStyle(
-                  fontFamily: 'sb',
-                  fontSize: 28,
                   color: AppColors.whiteColor,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(right: 16, left: 130),
-              child: Text(
-                '!خرید محصولات اپل',
-                style: TextStyle(
+                  fontSize: 18,
                   fontFamily: 'sb',
-                  fontSize: 28,
-                  color: AppColors.whiteColor,
                 ),
               ),
-            ),
-            Container(
-              height: 38,
-              margin: const EdgeInsets.only(
-                right: 22,
-                top: 32,
-                bottom: 32,
-                left: 300,
-              ),
-              decoration: BoxDecoration(
-                color: const Color(0xff253DEE),
-                shape: BoxShape.circle,
-                border: Border.all(
-                  strokeAlign: BorderSide.strokeAlignOutside,
-                  color: const Color(0xff253DEE),
-                  width: 10,
-                ),
-              ),
-              child: Image.asset('images/icon_arrow_right.png'),
-            ),
-            const Spacer(),
-            const Text(
-              'From',
-              style: TextStyle(
-                fontFamily: 'gb',
-                fontSize: 12,
-                color: Color(0xff86A5F8),
-              ),
-            ),
-            const Text(
-              'Shopping center',
-              style: TextStyle(
-                fontFamily: 'gb',
-                fontSize: 12,
-                color: AppColors.whiteColor,
-              ),
-            ),
+            )
           ],
         ),
       ),
--- lib/utils/constants/app_colors.dart
+++ lib/utils/constants/app_colors.dart
@@ -9,4 +9,5 @@ class AppColors {
   static const Color greenColor = Color(0xff1db68b);
   static const Color whiteColor = Color(0xffffffff);
   static const Color blackColor = Color(0xff000000);
+  static const Color blue = Color(0xff253DEE);
 }
